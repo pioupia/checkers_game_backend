69b44526675ee649ad3fdc9f6d96d9179c19b23c
Pioupia <pioupiadeux@gmail.com>
2023-06-06 21:04:48 +0200

    Feat: create the disconnect user behavior

--- src/index.ts
+++ src/index.ts
@@ -20,31 +20,57 @@ class Website extends WebSocketServer {
         this.on('connection', this.connection.bind(this));
     }
 
+    private connectPlayer(connection: Connection) {
+        this.waitingPlayer.push(connection);
+
+        if (this.waitingPlayer.length === 2) {
+            this.rooms.set(this.generateConnectionId(), this.waitingPlayer);
+
+            this.waitingPlayer[0].send('first');
+            this.waitingPlayer[1].send('second');
+
+            this.waitingPlayer = [];
+        }
+    }
+
+    private disconnectPlayer(connection: Connection) {
+        this.sessions.delete(connection);
+
+        for (let i = 0; i < this.waitingPlayer.length; i++) {
+            if (this.waitingPlayer[i].id === connection.id) {
+                this.waitingPlayer.splice(i, 1);
+                break;
+            }
+        }
+
+        for (const [key, players] of this.rooms.entries()) {
+            const index = players[0] === connection ?
+                0 : players[1] === connection ?
+                    1 : undefined;
+            if (index === undefined) continue;
+
+            const roomMate = players[index ? 0 : 1];
+            if (roomMate) {
+                this.connectPlayer(roomMate);
+                roomMate.send('disconnect');
+            }
+
+            this.rooms.delete(key);
+
+            break;
+        }
+    }
+
     private connection(ws: WebSocket.WebSocket) {
         const connection = new Connection(this.generateConnectionId(), ws);
         this.sessions.add(connection);
 
-        this.waitingPlayer.push(connection);
-        if (this.waitingPlayer.length === 2) {
-            this.rooms.set(this.generateConnectionId(), this.waitingPlayer);
-            this.waitingPlayer = [];
-        }
+        this.connectPlayer(connection);
 
         ws.on('error', connection.socketError.bind(connection));
         ws.on('message', connection.message.bind(connection));
-        ws.on('close', (code, reason) => {
-            this.sessions.delete(connection);
-
-            for (const [key, players] of this.rooms.entries()) {
-                const index = players[0] === connection ?
-                    0 : players[1] === connection ?
-                        1 : undefined;
-                if (index === undefined) continue;
-
-                players[index ? 0 : 1].send('disconnect');
-                this.rooms.delete(key);
-                break;
-            }
+        ws.on('close', () => {
+            this.disconnectPlayer(connection);
         });
     }
 
